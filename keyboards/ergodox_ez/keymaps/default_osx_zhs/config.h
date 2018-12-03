b9f91cded8ab5715af8d17fb995367f5faa48782
Sergiy Zhyrov <sergiy.zhyrov@touchcast.com>
2018-12-03 18:34:34 +0200

    [*] return `/keyboards/ergodox_ez/keymaps/default_osx_zhs`

---
+++ keyboards/ergodox_ez/keymaps/default_osx_zhs/config.h
@@ -0,0 +1,24 @@
+#ifndef CONFIG_USER_H
+#define CONFIG_USER_H
+
+#include "../../config.h"
+
+/*
+// 20
+#undef MOUSEKEY_TIME_TO_MAX
+#define MOUSEKEY_TIME_TO_MAX 30
+*/
+// 127
+#undef MOUSEKEY_MOVE_MAX
+#define MOUSEKEY_MOVE_MAX 500
+
+// 5
+#undef MOUSEKEY_MOVE_DELTA
+#define MOUSEKEY_MOVE_DELTA 10
+
+/*
+// 10
+#undef MOUSEKEY_MAX_SPEED
+#define MOUSEKEY_MAX_SPEED 11
+*/
+#endif
